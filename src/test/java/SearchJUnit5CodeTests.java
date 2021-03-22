d1c3cdff5ee694778b265a597fc8877da81ed183
Anton Babushking <napishi_antonu@mail.ru>
2021-03-22 17:28:43 +0300

    created searchJUnit5CodeTest

---
+++ src/test/java/SearchJUnit5CodeTests.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byName;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SearchJUnit5CodeTests {
+
+    @Test
+    void searchJUnit5CodeTest(){
+        //Открыть страницу
+        open("https://github.com/");
+        //Перейти в раздел Wiki
+        $(byName("q")).setValue("Selenide").pressEnter();
+        $$("ul.repo-list li").first().$("a").click();
+        $$("li.d-flex").find(text("Wiki")).click();
+        //Проверить наличие страницы и провалиться в нее
+        $("#wiki-pages-box").$(byText("SoftAssertions")).click();
+        //Проверить наличие кода для JUnit5
+        $(".markdown-body").shouldHave(text("Using JUnit5 extend test class"));
+    }
+}
